ea937c8b277be4013ffe92eb3b34b23d5e6c2ed3
Andrew Kaczorek <akaczorek@leafsoftwaresolutions.com>
2016-03-09 11:23:15 -0500

    pulled out pruning code from create-snapshots. This is now going in a standalone lambder called prune-snapshots

--- lambda/create-snapshots/snapper.py
+++ lambda/create-snapshots/snapper.py
@@ -37,53 +37,7 @@ class Snapper:
       Description=description
     )
 
-  def get_snapshots_to_delete(self, snapshots, max_to_keep=3):
-    snapshots_to_delete = []
-
-    if len(snapshots) >= max_to_keep:
-      # remove one extra to make room for the next snapshot
-      number_to_delete = len(snapshots) - max_to_keep + 1
-      snapshots_to_delete = snapshots[0:number_to_delete]
-
-    return snapshots_to_delete
-
-  # return a Dict() of {backupsource: list_of_snapshots}
-  def get_snapshots_by_backup_source(self):
-    filters = [{'Name':'tag-key', 'Values': [self.TAG_NAME]}]
-    snapshots = self.ec2.snapshots.filter(Filters=filters)
-
-    results = {}
-    for snapshot in snapshots:
-      tag = self.get_backup_source(snapshot)
-      if tag in results:
-        results[tag].append(snapshot)
-      else:
-        results[tag] = [snapshot]
-
-    for key in results.keys():
-      results[key] = sorted(results[key], key=lambda x: x.start_time)
-
-    return results
-
-  def prune(self):
-    pp = pprint.PrettyPrinter()
-    snapshots_by_source = self.get_snapshots_by_backup_source()
-
-    self.logger.debug('snapshots_by_source: ' + pp.pformat(snapshots_by_source))
-
-    for source in snapshots_by_source.keys():
-      all_snapshots = snapshots_by_source[source]
-      to_delete = self.get_snapshots_to_delete(all_snapshots)
-
-      self.logger.debug('to_delete: ' + pp.pformat(to_delete))
-
-      for condemned in to_delete:
-        self.logger.info("deleting " + condemned.snapshot_id)
-        condemned.delete()
-
   def run(self):
-    # prune old backups if needed
-    self.prune()
 
     # create new backups
     volumes = self.get_volumes_to_backup()
